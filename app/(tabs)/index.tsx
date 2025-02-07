f73232ef7dfa48a1017bc51f8fa6f4248c642d23
SGSANJAY044 <sgsanjay044@gmail.com>
2025-02-07 15:43:08 +0530

    feat(home): Implement dynamic student and class data fetching

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,63 @@
-import { Image, StyleSheet, Text,Platform, View } from 'react-native';
+import { Image, StyleSheet,ScrollView, Text,Platform, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Card from '@/components/Pages/Home/Card';
 import LeetBoard from '@/components/Pages/Home/LeetBoard';
 import { Shadow } from 'react-native-shadow-2';
+import { router } from 'expo-router';
+
+interface StudentData {
+  StudentID: number;
+  Streak: number;
+  SolvedEasy: number;
+  SolvedMedium: number;
+  SolvedHard: number;
+  Ranking: number;
+  Name: string;
+  ClassID: number;
+  Roll: string;
+  DepartmentID: number;
+  Phone: string;
+  Mail: string;
+  Username: string;
+}
 
 export default function HomeScreen() {
+  const [studentData, setStudentData] = useState<StudentData | null>(null);
+  const [classStudentsData, setClassStudentsData] = useState<StudentData[] | null>(null);
+
+  useEffect(() => {
+    const fetchStudentData = async () => {
+      try {
+        const response = await axios.get('https://f776-121-242-155-238.ngrok-free.app/students/21ADR044');
+        if (!response) {
+          throw new Error('Network response was not ok');
+        }
+        setStudentData(response.data.data);
+      } catch (error) {
+        console.error('Error fetching student data:', error);
+      }
+    };
+
+    const fetchClassStudentsData = async () => {
+      try {
+        const response = await axios.get('https://f776-121-242-155-238.ngrok-free.app/students/class/101');
+        console.log(response.data.students);
+        
+        if (!response) {
+          throw new Error('Network response was not ok');
+        }
+        setClassStudentsData(response.data.students);
+      } catch (error) {
+        console.error('Error fetching student data:', error);
+      }
+    };
+    fetchClassStudentsData();
+    fetchStudentData();
+  }, []);
   return (
     <View style={styles.body}>
-      <Card name='SANJAY' roll='21ADR044'/>
+      <Card name={studentData?.Name} roll={studentData?.Roll} easy={studentData?.SolvedEasy} medium={studentData?.SolvedMedium} hard={studentData?.SolvedHard}/>
       <View
       style={{
       borderBottomColor: 'grays',
@@ -24,9 +75,11 @@ export default function HomeScreen() {
         </View>
         </Shadow>
       </View>
-      <View style={styles.leadBoard}>
-      {Array.from({ length: 5 }, (_, i) => i + 1).map((index)=><LeetBoard rank={index} name='SANJAY' roll='21ADR044' total={(6-index)*233} />)}
+      <ScrollView style={{height: '60%',overflowY: 'scroll',}} showsVerticalScrollIndicator={false}  >
+      <View style={styles.leetBoard}>
+      {classStudentsData?.map((data,index)=><LeetBoard rank={index+1} name={data.Name} roll={data.Roll} total={(6-index)*233} key={index}/>)}
       </View>
+      </ScrollView>
       </View>
     </View>
   );
@@ -34,7 +87,7 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   body:{
-    marginTop:40,
+    marginTop:20,
     padding:20,
     gap:40
   },
@@ -52,8 +105,11 @@ const styles = StyleSheet.create({
     borderRadius:5,
     padding:5
   },
-  leadBoard:{
-    gap:20
+  leetBoard:{
+    gap:20,
+    display: 'flex',
+    height:'100%',
+    overflowY:'scroll',
   },
   card:{
     display:'flex',
--- app/(tabs)/schedule.tsx
+++ app/(tabs)/schedule.tsx
@@ -1,8 +1,9 @@
 
-import { Image, StyleSheet, Text,Platform, View } from 'react-native';
+import { Image, StyleSheet, ScrollView, Text,Platform, View } from 'react-native';
 import TaskCard from '@/components/Pages/Schedule/TaskCard';
 export default function schedule() {
   return (
+    <ScrollView>
    <View style={styles.body}>
     <View>
       <Text style={{fontSize:20,fontWeight:600,color:'#EBA340',marginBottom:20}}>TODAY LEET TASKS</Text>
@@ -10,20 +11,23 @@ export default function schedule() {
               {Array.from({ length: 3 }, (_, i) => i + 1).map((index)=><TaskCard number={82} question="ADD TWO NUMBERS" type="Hard" finished={true}/>)}
       </View>
     </View>
-    <View>
+    <View style={{paddingBottom:20}}>
       <Text style={{fontSize:20,fontWeight:600,color:'#EBA340',marginBottom:20}}>FRIENDS QUESTIONS</Text>
       <View style={{gap:20}}>
               {Array.from({ length: 5 }, (_, i) => i + 1).map((index)=><TaskCard number={82} question="ADD TWO NUMBERS" type="Hard" finished={true}/>)}
       </View>
     </View>
     </View>
+    </ScrollView>
   )
 }
 
 const styles=StyleSheet.create({
   body:{
-    marginTop:40,
+    marginTop:20,
     padding:20,
-    gap:40
+    gap:40,
+    height: '100%',
+    overflowY: 'scroll'
   }
 })
