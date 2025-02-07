83d5addb842116bee2005b977e2b37137406c115
SGSANJAY044 <sgsanjay044@gmail.com>
2025-02-07 15:44:03 +0530

    feat(auth): Add login and signup screens with authentication flow

---
+++ app/(auth)/index.tsx
@@ -0,0 +1,116 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View,Image, TextInput, TouchableOpacity } from 'react-native';
+import { router } from 'expo-router';
+import axios from 'axios';
+import Toast from 'react-native-toast-message';
+export default function Login() {
+  const [mail, setMail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async() => {
+    try {
+      const responce =await axios.post('https://f776-121-242-155-238.ngrok-free.app/auth/student/login',{mail,password});
+      console.log(responce);
+      
+    } catch (error) {
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: error
+      });
+      console.error('Signup65 failed:', error);
+      return false;
+    }
+    // On successful login:
+    router.replace('/(tabs)');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.background}>
+      <Image style={styles.logo} source={require('@/assets/images/logo.png')} />
+      <Text style={{fontSize:45,fontWeight:600,color:'#fff',marginBottom:100}}>LEETLEADER</Text>
+      </View>
+      <View style={{marginTop:450}}>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={mail}
+        onChangeText={setMail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity onPress={() => router.push('/(auth)/signup')}>
+        <Text style={styles.linkText}>Don't have an account? Sign up</Text>
+      </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  logo:{
+    height:210,
+    width: 210,
+    marginBottom:40
+
+  },
+  background:{
+    top: -250,
+    left: -150,
+    backgroundColor:'#EBA340',
+    height:700,
+    width:700,
+    borderRadius:'100%',
+    position: 'absolute',
+    alignItems:'center',
+    justifyContent:'flex-end'
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+    color: '#EBA340',
+  },
+  input: {
+    backgroundColor: 'white',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#EBA340',
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  linkText: {
+    color: '#EBA340',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+});
---
+++ app/(auth)/signup.tsx
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+import { StyleSheet, Text,Image, View, TextInput, TouchableOpacity } from 'react-native';
+import { router } from 'expo-router';
+import axios from 'axios';
+
+export default function Signup() {
+  const [mail, setMail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = async() => {
+    try {
+      await axios.post('https://f776-121-242-155-238.ngrok-free.app/auth/student/signup',{mail,password});
+    } catch (error) {
+      console.error('Signup failed:', error);
+      return false;
+    }
+    // On successful signup:
+    router.replace('/(tabs)');
+  };
+
+  return (
+    <View style={styles.container}>
+    <View style={styles.background}>
+    <Image style={styles.logo} source={require('@/assets/images/logo.png')} />
+    <Text style={{fontSize:45,fontWeight:600,color:'#fff',marginBottom:100}}>LEETLEADER</Text>
+    </View>
+    <View style={{marginTop:450}}>
+    <TextInput
+      style={styles.input}
+      placeholder="Email"
+      value={mail}
+      onChangeText={setMail}
+      keyboardType="email-address"
+      autoCapitalize="none"
+    />
+    
+    <TextInput
+      style={styles.input}
+      placeholder="Password"
+      value={password}
+      onChangeText={setPassword}
+      secureTextEntry
+    />
+    
+    <TouchableOpacity style={styles.button} onPress={handleSignup}>
+      <Text style={styles.buttonText}>Signup</Text>
+    </TouchableOpacity>
+    
+    <TouchableOpacity onPress={() => router.push('/(auth)')}>
+      <Text style={styles.linkText}>Already have an account? Login</Text>
+    </TouchableOpacity>
+    </View>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    logo:{
+      height:210,
+      width: 210,
+      marginBottom:40
+  
+    },
+    background:{
+      top: -250,
+      left: -150,
+      backgroundColor:'#EBA340',
+      height:700,
+      width:700,
+      borderRadius:'100%',
+      position: 'absolute',
+      alignItems:'center',
+      justifyContent:'flex-end'
+    },
+    container: {
+      flex: 1,
+      padding: 20,
+      justifyContent: 'center',
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+      textAlign: 'center',
+      color: '#EBA340',
+    },
+    input: {
+      backgroundColor: 'white',
+      padding: 15,
+      borderRadius: 10,
+      marginBottom: 20,
+    },
+    button: {
+      backgroundColor: '#EBA340',
+      padding: 15,
+      borderRadius: 10,
+      marginTop: 10,
+    },
+    buttonText: {
+      color: 'white',
+      textAlign: 'center',
+      fontWeight: 'bold',
+    },
+    linkText: {
+      color: '#EBA340',
+      textAlign: 'center',
+      marginTop: 20,
+    },
+  });
