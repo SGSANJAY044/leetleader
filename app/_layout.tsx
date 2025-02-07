bf7fd21338875f741601bfb4a98c6d2723eaa666
SGSANJAY044 <sgsanjay044@gmail.com>
2025-02-07 15:43:37 +0530

    chore(app): Add expo-font plugin and toast message integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import Toast from 'react-native-toast-message';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -28,7 +28,9 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
+      <Toast/>
+      <Stack screenOptions={{ headerShown: false }} >
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} redirect={false} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
